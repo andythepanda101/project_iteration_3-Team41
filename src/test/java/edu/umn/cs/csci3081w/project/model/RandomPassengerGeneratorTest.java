3bc5c5ac20e14bc6f3b7a9ac67eb1e8364afc8b0
Julia Pan <pan00079@umn.edu>
2021-12-06 00:55:36 -0600

    Adding initial tests for model classes

--- src/test/java/edu/umn/cs/csci3081w/project/model/RandomPassengerGeneratorTest.java
+++ src/test/java/edu/umn/cs/csci3081w/project/model/RandomPassengerGeneratorTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 public class RandomPassengerGeneratorTest {
 
   private RandomPassengerGenerator testRandomPassengerGenerator;
-
+  private RandomPassengerGenerator testRandomPassengerGeneratorProbabilities;
 
   /**
    * Setup operations before each test runs.
@@ -28,8 +28,13 @@ public class RandomPassengerGeneratorTest {
 
     List<Double> probabilities = new ArrayList<Double>();
     probabilities.add(.15);
-
     testRandomPassengerGenerator = new RandomPassengerGenerator(stops, probabilities);
+
+    List<Double> probabilitiesM = new ArrayList<Double>();
+    probabilitiesM.add(.60);
+    probabilitiesM.add(.30);
+    probabilitiesM.add(.15);
+    testRandomPassengerGeneratorProbabilities = new RandomPassengerGenerator(stops, probabilitiesM);
 
   }
 
@@ -57,12 +62,40 @@ public class RandomPassengerGeneratorTest {
    */
   @Test
   public void testGeneratePassengers() {
-
     testRandomPassengerGenerator.DETERMINISTIC = true;
     assertEquals(1, testRandomPassengerGenerator.generatePassengers());
-
   }
 
+  /**
+   * Test generatePassengers function with more probabilities.
+   */
+  @Test
+  public void testGeneratePassengersMoreProbabilities() {
+    testRandomPassengerGeneratorProbabilities.DETERMINISTIC = true;
+    assertEquals(4, testRandomPassengerGeneratorProbabilities.generatePassengers());
+    assertEquals(4, testRandomPassengerGeneratorProbabilities.generatePassengers());
+    assertEquals(4, testRandomPassengerGeneratorProbabilities.generatePassengers());
+  }
+
+  /**
+   * Test generatePassengers function if it's not deterministic.
+   * Deterministic test with our configuration always generates exactly 1 passenger,
+   * so, to test if the non-deterministic version is working, we can check the values
+   * we get on 50 different trials. If any of then is not 1, then it already deviates
+   * from the deterministic version.
+   */
+  @Test
+  public void testGeneratePassengerNonDeterministic() {
+    testRandomPassengerGeneratorProbabilities.DETERMINISTIC = false;
+    boolean same = true;
+    for (int i = 0; i < 50; i++) {
+      if (testRandomPassengerGenerator.generatePassengers() != 1) {
+        same = false;
+        break;
+      }
+    }
+    assertEquals(false, same);
+  }
 
   /**
    * Clean up our variables after each test.
